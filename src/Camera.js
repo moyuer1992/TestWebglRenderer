73e7818b8fda64002bd50837664322b885c50356
xuepei <xuepei_paint@163.com>
2021-09-16 10:44:35 +0800

    camera struct change

--- src/Camera.js
+++ src/Camera.js
@@ -2,21 +2,29 @@ import Matrix4 from './Matrix4'
 
 class Camera {
   constructor () {
-    this.pos = [0, 0, 0]
+    this.position = [0, 0, 0]
     this.at = [0, 0, 0]
     this.up = [0, 1, 0]
     this.viewMatrix = new Matrix4().setLookAt(
-      this.pos[0], this.pos[1], this.pos[2],
+      this.position[0], this.position[1], this.position[2],
       this.at[0], this.at[1], this.at[2],
       this.up[0], this.up[1], this.up[2],
     )
     this.projectionMatrix = new Matrix4()
   }
-  lookAt (eyeX, eyeY, eyeZ, atX, atY, atZ, upX, upY, upZ) {
-    this.pos = [eyeX, eyeY, eyeZ]
+  lookAt (atX, atY, atZ) {
     this.at = [atX, atY, atZ]
-    this.up = [upX, upY, upZ]
-    this.viewMatrix = new Matrix4().setLookAt(eyeX, eyeY, eyeZ, atX, atY, atZ, upX, upY, upZ)
+    this.viewMatrix = new Matrix4().setLookAt(
+      this.position[0],
+      this.position[1],
+      this.position[2],
+      atX,
+      atY,
+      atZ,
+      this.up[0],
+      this.up[1],
+      this.up[2],
+    )
     return this.viewMatrix
   }
 }
