b187d2a8396cb933b961448253e1e109a8f73540
xuepei <xuepei_paint@163.com>
2021-09-19 16:32:11 +0800

    ortho func changed

--- src/Matrix4.js
+++ src/Matrix4.js
@@ -3,8 +3,8 @@ class Matrix4 {
     this.elements = new Float32Array([1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1])
     if (elements) this.set(elements)
   }
-  set (src) {
-    let e_arr = src.elements
+  set (elements) {
+    let e_arr = elements
     for (let i = 0; i < 16; i++) {
       this.elements[i] = e_arr[i]
     }
@@ -225,40 +225,54 @@ class Matrix4 {
 
     return this
   }
+//   setOrtho (left, right, bottom, top, near, far) {
+//     var e, rw, rh, rd
+// 
+//     if (left === right || bottom === top || near === far) {
+//       throw 'null frustum'
+//     }
+// 
+//     rw = 1 / (right - left)
+//     rh = 1 / (top - bottom)
+//     rd = 1 / (far - near)
+// 
+//     e = this.elements
+// 
+//     e[0]  = 2 * rw
+//     e[1]  = 0
+//     e[2]  = 0
+//     e[3]  = 0
+// 
+//     e[4]  = 0
+//     e[5]  = 2 * rh
+//     e[6]  = 0
+//     e[7]  = 0
+// 
+//     e[8]  = 0
+//     e[9]  = 0
+//     e[10] = -2 * rd
+//     e[11] = 0
+// 
+//     e[12] = -(right + left) * rw
+//     e[13] = -(top + bottom) * rh
+//     e[14] = -(far + near) * rd
+//     e[15] = 1
+// 
+//     return this
+//   }
+
   setOrtho (left, right, bottom, top, near, far) {
-    var e, rw, rh, rd
+    const width = right - left;
+    const height = top - bottom;
+    const depth = far - near;
 
-    if (left === right || bottom === top || near === far) {
-      throw 'null frustum'
-    }
-
-    rw = 1 / (right - left)
-    rh = 1 / (top - bottom)
-    rd = 1 / (far - near)
-
-    e = this.elements
-
-    e[0]  = 2 * rw
-    e[1]  = 0
-    e[2]  = 0
-    e[3]  = 0
-
-    e[4]  = 0
-    e[5]  = 2 * rh
-    e[6]  = 0
-    e[7]  = 0
-
-    e[8]  = 0
-    e[9]  = 0
-    e[10] = -2 * rd
-    e[11] = 0
-
-    e[12] = -(right + left) * rw
-    e[13] = -(top + bottom) * rh
-    e[14] = -(far + near) * rd
-    e[15] = 1
-
-    return this
+    this.set(new Float32Array([
+      2 / width,               0.0,                      0.0,                   0.0,
+      0.0,                     2 / height,               0.0,                   0.0,
+      0.0,                     0.0,                      2 / depth,            0.0,
+      -(left + right) / width, -(top + bottom) / height, -(near + far) / depth, 1.0
+    ]));
+    return this;
   }
 }
 
